76976e661fd8b188e3f0853fd313a9cae87be01d
Utkarsh Mishra <86395901+Utkarsh1929@users.noreply.github.com>
2026-06-07 03:30:02 +0530

    Update 1545_Find_Kth_Bit_in_Nth_Binary_String.java

--- String/medium/1545_Find_Kth_Bit_in_Nth_Binary_String.java
+++ String/medium/1545_Find_Kth_Bit_in_Nth_Binary_String.java
@@ -27,5 +27,24 @@ Constraints:
 1 <= n <= 20
 1 <= k <= 2n - 1   */
 
+class Solution {
+    public char findKthBit(int n, int k) {
+        if(n==1){
+            return '0';
+        }
 
+        int l = (1<<n)-1;
+        int mid = l/2+1;
+
+        if(k==mid){
+            return '1';
+        }
+        else if(k<mid){
+            return findKthBit(n-1, k);
+        }else{
+            char symmetricBit = findKthBit(n-1, l-k+1);
+            return symmetricBit=='0'?'1':'0';
+        }
+    }
+}
 
